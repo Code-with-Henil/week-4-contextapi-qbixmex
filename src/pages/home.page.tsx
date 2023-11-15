c072389092e652764f99ad406f421a1a4ee0ee90
Daniel González Briseño <qbixmex@gmail.com>
2023-11-15 01:04:34 -0800

    some-fixes

---
+++ src/pages/home.page.tsx
@@ -0,0 +1,13 @@
+import { Box, Typography } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <Box sx={{ textAlign: 'center' }}>
+      <Typography component="h1" fontWeight="500" fontSize="5rem">
+        Currency Exchange
+      </Typography>
+    </Box>
+  );
+};
+
+export default HomePage;
